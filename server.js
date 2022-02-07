679d55c3f549a95a6912458954f66104470161cc
Jeffrey Rose <jtr219@outlook.com>
2022-02-07 16:26:15 -0500

    set up routes

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Budget = require("./models/Budget");
 
 const main = async () => {
+  // initialize app
   const app = express();
 
+  // connect to mongo
+  mongoose.connect("mongodb://localhost:27017/fruits", () => {
+    console.log("mongoose connected");
+  });
+
+  // routes
+  app.get("/budgets", async (_req, res) => {
+    res.send("Index Page");
+  });
+
+  // form to add a new budget item
+  app.get("/budgets/new", (_req, res) => {
+    res.send("add new item");
+  });
+
+  // post method to receive the above form and update the database
+  app.post("/budgets", (req, res) => {
+    res.send("posted!");
+  });
+
+  app.get("/budgets/:id", async (req, res) => {
+    res.send("individual item");
+  });
+
+  // start server
   app.listen(3000, () => {
     console.log("app listening on port 3000");
   });
